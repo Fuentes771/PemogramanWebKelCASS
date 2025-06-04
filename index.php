ad6a6fe135c076b21794a2207dda7f04a0a9485f
Akeyla <2315061070@students.unila.ac.id>
2025-06-04 21:31:55 +0700

    sinkron

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ session_start();
 
   <section class="hero">
     <div class="hero-text">
-      <p class="subtitle">Awali harimu dengan sempurna bersama kopi spesial dari kami.</p>
       <h1 class="title">Kopi & Kuki</h1>
       <p class="description">
       Kopi & Kuki adalah tempat di mana aroma kopi segar berpadu sempurna dengan manisnya kuki hangat. Temukan kenyamanan dalam setiap tegukan dan gigitan.

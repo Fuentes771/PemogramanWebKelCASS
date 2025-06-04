3cc5572eafb39f5e423c424c96f49c255b52975f
Fuentes <sulton843@gmail.com>
2025-06-04 18:45:16 +0700

    Perubahan Nama Website

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ session_start();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Bean Scene</title>
+  <title>Choco & Beans</title>
   <link rel="stylesheet" href="css/style.css" />
   <link rel="stylesheet" href="css/navbar.css" />
 </head>
 <body>
   <header class="navbar">
-    <div class="logo">Bean Scene</div>
+    <div class="logo">Choco & Beans</div>
     <nav>
       <a href="index.php">Home</a>
       <a href="menu.php">Menu</a>

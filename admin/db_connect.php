7bc9bfbb9991f90b7c8c4aee24221ff0d34b13b9
Nabila Salwa <nabilasalwaal2105@gmail.com>
2025-06-06 15:01:39 +0700

    Membuat keterangan order dinamis

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,1 +1,12 @@
-include '../db_connect.php';
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "toko_kopi";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+?>
+
